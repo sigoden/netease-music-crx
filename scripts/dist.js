c32cedee20ccbc7f442f6d5b2a856e8e630a1eb1
sigoden <sigoden@gmail.com>
2018-01-16 21:46:09 +0800

    修复打包crx文件损坏异常

--- scripts/dist.js
+++ scripts/dist.js
@@ -6,22 +6,33 @@ const fs = require('fs')
 const PEM_FILE = process.env.PEM_FILE || './build.pem'
 const path = require('path')
 const paths = require('../config/paths')
-const manifest = require(path.join(paths.appBuild, 'manifest.json'))
-const outputFile = path.join(paths.appDist, `netease-music-crx_v${manifest.version}`)
-const cp = require('child_process');
+const cp = require('child_process')
+
 
 if (!fs.existsSync(PEM_FILE)) {
   console.error(`Invalid pem files: ${PEM_FILE}`)
   process.exit(1)
 }
 
-const cmd = `npx crx pack ${paths.appBuild} -o ${outputFile}.crx --zip-output ${outputFile}.zip -p ${PEM_FILE}`
+let manifestPath = path.join(paths.appBuild, 'manifest.json')
+let manifestContent = fs.readFileSync(manifestPath, {encoding: 'utf8'})
+let manifest = JSON.parse(manifestContent)
+let outputFile = path.join(paths.appDist, `netease-music-crx_v${manifest.version}`)
+
+let cmd = `npx crx pack ${paths.appBuild} --zip-output ${outputFile}.zip -o ${outputFile}.crx -p ${PEM_FILE}`
 cp.exec(cmd, function(err, stdout, stderr) {
   if (err) throw err
   if (stderr) {
-    console.error(stdout);
+    console.error(stdout)
   }
-  if (stdout) {
-    console.log(stdout);
-  }
+  manifest.update_url = "http://localhost:3000/update2/crx"
+  fs.writeFileSync(manifestPath, JSON.stringify(manifest))
+  cmd = `npx crx pack ${paths.appBuild} -o ${outputFile}.crx -p ${PEM_FILE}`
+  cp.exec(cmd, function(err, stdout, stderr) {
+    fs.writeFileSync(manifestPath, manifestContent)
+    if (err) throw err
+    if (stderr) {
+      console.error(stdout)
+    }
+  })
 })
