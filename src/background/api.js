7ed31070450d348cc4076a73d38cedcf81e6e49f
sigoden <sigoden@gmail>
2021-11-02 07:41:16 +0800

    refactor api

--- src/background/api.js
+++ src/background/api.js
@@ -1,7 +1,13 @@
+import crypto from 'crypto'
+import bigInt from 'big-integer'
 import { DOMAIN, log } from '../utils'
-import * as encrypt from './encrypt'
 
 export default createRequester()
+
+const MODULUS =
+  '00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7'
+const NONCE = '0CoJUm6Qyw8W8jud'
+const PUBKEY = '010001'
 
 function createRequester () {
   function createRequest (reqInfo) {
@@ -133,9 +139,55 @@ function createRequester () {
 }
 
 function createQueryParams (data) {
-  const cryptoReq = encrypt.encryptData(data)
+  const cryptoReq = encryptData(data)
   const body = new URLSearchParams()
   body.append('params', cryptoReq.params)
   body.append('encSecKey', cryptoReq.encSecKey)
   return body
+}
+
+function encryptData (obj) {
+  const text = JSON.stringify(obj)
+  const secKey = createSecretKey(16)
+  const encText = aesEncrypt(aesEncrypt(text, NONCE), secKey)
+  const encSecKey = rsaEncrypt(secKey, PUBKEY, MODULUS)
+  return {
+    params: encText,
+    encSecKey: encSecKey
+  }
+}
+
+function createSecretKey (size) {
+  const keys = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  let key = ''
+  for (let i = 0; i < size; i++) {
+    let pos = Math.random() * keys.length
+    pos = Math.floor(pos)
+    key = key + keys.charAt(pos)
+  }
+  return key
+}
+
+function aesEncrypt (text, secKey) {
+  const _text = text
+  const lv = Buffer.from('0102030405060708', 'binary')
+  const _secKey = Buffer.from(secKey, 'binary')
+  const cipher = crypto.createCipheriv('AES-128-CBC', _secKey, lv)
+  let encrypted = cipher.update(_text, 'utf8', 'base64')
+  encrypted += cipher.final('base64')
+  return encrypted
+}
+
+function rsaEncrypt (text, pubKey, modulus) {
+  const _text = text.split('').reverse().join('')
+  const biText = bigInt(Buffer.from(_text).toString('hex'), 16)
+  const biEx = bigInt(pubKey, 16)
+  const biMod = bigInt(modulus, 16)
+  const biRet = biText.modPow(biEx, biMod)
+  return zfill(biRet.toString(16), 256)
+}
+
+function zfill (str, size) {
+  while (str.length < size) str = '0' + str
+  return str
 }
