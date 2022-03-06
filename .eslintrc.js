a76b44fdd740abc064c1232d4fa584ddce718df6
sigoden <sigoden@gmail.com>
2022-03-06 14:42:03 +0800

    build: update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ process.env.NODE_ENV = "development";
 module.exports = {
   parser: "@babel/eslint-parser",
   extends: ["react-app", "plugin:prettier/recommended"],
-  plugins: ["react"],
   globals: {
     chrome: "readonly",
     globalThis: true,
